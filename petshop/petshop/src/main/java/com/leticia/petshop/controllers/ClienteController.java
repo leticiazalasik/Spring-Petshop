c10bdf369d5a19e841e6ee9ffac676542d38b771
Letícia Zalasik <leticiazalasik@gmail.com>
2024-05-18 09:53:32 -0300

    :sparkles: Feat (service, controller) : Criadas as classes ClienteService e ClienteControler.

---
+++ petshop/petshop/src/main/java/com/leticia/petshop/controllers/ClienteController.java
@@ -0,0 +1,47 @@
+package com.leticia.petshop.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leticia.petshop.models.Cliente;
+import com.leticia.petshop.service.ClienteService;
+
+@RestController
+@RequestMapping("/api")
+public class ClienteController {
+
+@Autowired
+ClienteService clienteService; 
+
+@GetMapping("/clientes")
+public List<Cliente>listarClientes(){
+return clienteService.listarClientes(); 
+}
+
+@PostMapping("/clientes")
+public ResponseEntity<Cliente>cadastrarCliente(@RequestBody Cliente cliente){
+return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.criarCliente(cliente));	
+}
+
+@PutMapping("/clientes/{id}")
+public ResponseEntity<Cliente>atualizarCliente(@PathVariable Long id, @RequestBody Cliente cliente){
+return ResponseEntity.status(HttpStatus.OK).body(clienteService.atualizarCliente(id, cliente));	
+}
+@DeleteMapping("clientes/{id}")
+public ResponseEntity<Object> excluirCliente(@PathVariable Long id ){
+return ResponseEntity.status(HttpStatus.OK).body("Cliente excluído com sucesso");	
+}
+	
+}
---
+++ petshop/petshop/src/main/java/com/leticia/petshop/service/ClienteService.java
@@ -0,0 +1,35 @@
+package com.leticia.petshop.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leticia.petshop.models.Cliente;
+import com.leticia.petshop.repository.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	
+@Autowired
+private ClienteRepository clienteRepository; 
+
+public Cliente criarCliente(Cliente cliente) { 
+return clienteRepository.save(cliente); 
+}
+
+public List<Cliente>listarClientes(){ 
+	return clienteRepository.findAll();
+}
+
+public Cliente atualizarCliente (Long id, Cliente cliente) {
+cliente.setIdCliente(id);
+return clienteRepository.save(cliente); 
+}
+
+public void excluirCliente(Long id) { 
+	clienteRepository.deleteById(id);
+	System.out.println("Ecluidno cliente de ID: "+ id);
+}
+}
